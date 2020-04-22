eac8f3d068aa4bb19500482e043703accf6c4976
Micah Smith <micahjsmith@gmail.com>
2020-04-22 13:16:49 -0400

    Add unimplemented routes

--- ballet_oauth_gateway/views.py
+++ ballet_oauth_gateway/views.py
@@ -1,7 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 blueprint = Blueprint('main', __name__)
+
 
 @blueprint.route('/status')
 def status():
     return 'OK'
+
+
+@blueprint.route('/app_id')
+def app_id():
+    return 0
+
+
+@blueprint.route('/authorize')
+def authorize():
+    code = request.args.get('code')
+    state = request.args.get('state')
+    print(f'code: {code}')
+    print(f'state: {state}')
+
+
+@blueprint.route('/access_code', methods=['POST'])
+def access_code():
+    state = request.form.get('state')
+    print(f'state: {state}')
