11f3f4812ee73bc4ce61b4fafa1069e0e8baabca
Micah Smith <micahjsmith@gmail.com>
2020-04-28 10:29:43 -0400

    Serve from wsgi:app

--- tasks.py
+++ tasks.py
@@ -3,3 +3,13 @@ from invoke import task
 @task
 def test(c):
     c.run('python -m pytest')
+
+
+@task
+def lint(c):
+    c.run('flake8 ballet_oauth_gateway tests')
+
+
+@task
+def serve(c):
+    c.run('gunicorn wsgi:app')
